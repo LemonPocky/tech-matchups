03ecb59f5849a31ae64aa0fc8181c725541094c9
LemonPocky <someone-70@live.com>
2021-08-19 10:25:43 -0700

    added apollo client

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,45 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Home from "./pages/Home";
-import Matchup from "./pages/Matchup";
-import Vote from "./pages/Vote";
-import NotFound from "./pages/NotFound";
+import React from 'react';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './pages/Home';
+import Matchup from './pages/Matchup';
+import Vote from './pages/Vote';
+import NotFound from './pages/NotFound';
 
 // ----------------------------------------------------------------------------
 // TODO: create an ApolloClient instance.
 // ...
 // ----------------------------------------------------------------------------
+const client = new ApolloClient({
+  uri: '/graphql',
+  cache: new InMemoryCache(),
+});
 
 // TODO: update App so that all components are rendered as descendants of an
 // ApolloProvider component using the ApolloClient instance for the client prop
 // on the ApolloProvider.
 function App() {
   return (
-    <Router>
-      <div className="flex-column justify-center align-center min-100-vh bg-primary">
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route exact path="/matchup">
-            <Matchup />
-          </Route>
-          <Route exact path="/matchup/:id">
-            <Vote />
-          </Route>
-          <Route>
-            <NotFound />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+    <ApolloProvider client={client}>
+      <Router>
+        <div className="flex-column justify-center align-center min-100-vh bg-primary">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route exact path="/matchup">
+              <Matchup />
+            </Route>
+            <Route exact path="/matchup/:id">
+              <Vote />
+            </Route>
+            <Route>
+              <NotFound />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </ApolloProvider>
   );
 }
 
